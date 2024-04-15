13ac370d94682b0a5967f332cb9732962ee2867d
Olivia Bahr <ob.bahr@gmail.com>
2024-04-15 11:22:39 -0600

    update personal anno files

--- manual_annotation/nick/nick_anno.py
+++ manual_annotation/nick/nick_anno.py
@@ -1,8 +1,9 @@
-import os
+from os.path import dirname, basename, abspath
+from glob import glob
 from pandas import DataFrame, read_csv
 
 def print_progress(name):
-    csv_files = [f for f in os.listdir() if f.endswith('.csv')]
+    csv_files = glob(f'{DIR}/*.csv')
 
     for f in csv_files:
         df = read_csv(f)
@@ -13,10 +14,12 @@ def print_progress(name):
             numLabeled = len(df[df['sentiment'].isin([1, 2, 3])])
 
         # print progress
-        print(f'{f.split(".")[0]}: {numLabeled}/{len(df)}')
+        print(f'{basename(f).split(".")[0]}: {numLabeled}/{len(df)}')
+    
 
 def tater_loop(df) -> DataFrame:
     for i, row in df.iterrows():
+
         # skip row if already annotated
         if row['sentiment'] in [1, 2, 3]:
             continue
@@ -28,37 +31,36 @@ def tater_loop(df) -> DataFrame:
         # handle input
         while True:
             sentiment = input('\n1: neu, 2: pos, 3: neg, q: save and exit\nsentiment: ')
-            if sentiment == 'q':
-                print('\nsaving and quitting')
-                return df
-            elif sentiment in ('1', '2', '3'):
-                df.at[i, 'sentiment'] = int(sentiment)
-                print(f'labeled chunk {row["chunk"]} as {sentiment}\n')
-                break
-            else:
-                print('Invalid input')
+            match sentiment:
+                case 'q':
+                    print('\nsaving and quitting')
+                    return df
+                case '1' | '2' | '3':
+                    df.at[i, 'sentiment'] = int(sentiment)
+                    print(f'labeled chunk {row["chunk"]} as {sentiment}\n')
+                    break
+                case _:
+                    print('Invalid input')
 
     print('Finished annotating!')
     return df
 
+
 def annotate(name):
+
     print_progress(name)
 
     # input bible name
-    bibname = input("\nEnter bible to annotate (e.g. asv, q to quit): ")
-    if bibname == 'q':
+    bibName = input("\nEnter bible to annotate (e.g. asv, q to quit): ")
+    if bibName == 'q':
         print('🫡')
         return
 
-    # append '.csv' to bibname
-    path = bibname + '.csv'
-
     # read csv
-    # path = os.path.join(name, bibname)
-    # print(path)
-    df = read_csv(path)
+    bibPath = glob(f'{DIR}/{bibName}.csv')[0]
+    df = read_csv(bibPath)
 
-    # add empty sentiment column
+    #add empty sentiment column
     if 'sentiment' not in df.columns:
         df['sentiment'] = 0
 
@@ -66,12 +68,15 @@ def annotate(name):
     df = tater_loop(df)
 
     # save
-    df.to_csv(path, index=False)
-    print(f'Saved to {path}\n')
+    df.to_csv(bibPath, index=False)
+    print(f'Saved to {bibPath}\n')
 
     # continue
     annotate(name)
 
+
 if __name__ == '__main__':
+    DIR = dirname(abspath(__file__))
+    
     # add your name in the argument
     annotate('nick')
